0a1c8f115e277d9952ba27e67b38a855f5d61ab9
ankitmankar0 <ankitmankar012@gmail.com>
2022-04-09 13:08:05 +0530

    Added New Contact

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -16,3 +16,25 @@ AddressBookType varchar(50),
 AddressBookName varchar(50) 
 );
 select * from AddressBookSystem;
+
+DELETE FROM AddressBookSystem WHERE FirstName='Shubham';
+
+--insert record into table
+insert into AddressBookSystem values('Ankit','Mankar','Wadi','Nagpur','MH','440023','8888888888','aron@gmail.com','Family','FamilyBook');
+select * from AddressBookSystem;
+
+--UC3
+Create procedure dbo.AddressBookSystemProcedure
+	@FirstName  varchar(150),		
+	@LastName  varchar(150),	
+	@Address  varchar(150),		
+	@City	varchar(150),	
+	@State	varchar(150),	
+	@Zip	varchar(6),
+    @PhoneNumber varchar(50),
+	@EmailId	varchar(150),	
+	@AddressBookType	varchar(150),
+	@AddressBookName	varchar(150)
+as begin
+	Insert into AddressBookSystem values(@FirstName,@LastName,@Address,@City,@State,@Zip,@PhoneNumber,@EmailId,@AddressBookType,@AddressBookName)
+End
